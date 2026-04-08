f5a43817469a3a81b011a583a00d3b8218ec48dc
JeremyH76 <>
2026-04-08 16:58:34 +0200

    Add lot of logs

--- pkg/tcp/request.go
+++ pkg/tcp/request.go
@@ -17,14 +17,18 @@ var log zerolog.Logger
 
 // Do - http.Client with support Digest Authorization
 func Do(req *http.Request) (*http.Response, error) {
+	log.Trace().Str("method", req.Method).Str("url", req.URL.String()).Msg("[tcp] http request")
+
 	var secure *tls.Config
 
 	switch req.URL.Scheme {
 	case "httpx":
+		log.Debug().Str("url", req.URL.String()).Msg("[tcp] httpx scheme: forcing insecure TLS")
 		secure = insecureConfig
 		req.URL.Scheme = "https"
 	case "https":
 		if hostname := req.URL.Hostname(); IsIP(hostname) {
+			log.Debug().Str("host", hostname).Msg("[tcp] https with IP address: using insecure TLS")
 			secure = insecureConfig
 		}
 	}
@@ -39,20 +43,27 @@ func Do(req *http.Request) (*http.Response, error) {
 
 		dial := transport.DialContext
 		transport.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+			log.Trace().Str("network", network).Str("addr", addr).Msg("[tcp] dial")
 			conn, err := dial(ctx, network, addr)
+			if err != nil {
+				log.Error().Err(err).Str("addr", addr).Msg("[tcp] dial failed")
+			}
 			if pconn, ok := ctx.Value(connKey).(*net.Conn); ok {
 				*pconn = conn
 			}
 			return conn, err
 		}
 		transport.DialTLSContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+			log.Trace().Str("network", network).Str("addr", addr).Msg("[tcp] TLS dial")
 			conn, err := dial(ctx, network, addr)
 			if err != nil {
+				log.Error().Err(err).Str("addr", addr).Msg("[tcp] TLS dial failed")
 				return nil, err
 			}
 
 			var conf *tls.Config
 			if v, ok := ctx.Value(secureKey).(*tls.Config); ok {
+				log.Trace().Str("addr", addr).Msg("[tcp] using insecure TLS config")
 				conf = v
 			} else if host, _, err := net.SplitHostPort(addr); err != nil {
 				conf = &tls.Config{ServerName: addr}
@@ -61,24 +72,26 @@ func Do(req *http.Request) (*http.Response, error) {
 			}
 
 			tlsConn := tls.Client(conn, conf)
-		if err = tlsConn.Handshake(); err != nil {
-			// retry with TLS 1.2 for cameras that don't support TLS 1.3
-			log.Warn().Err(err).Str("addr", addr).Msg("[tcp] TLS 1.3 handshake failed, retrying with TLS 1.2")
-			_ = tlsConn.Close()
-			conn, err = dial(ctx, network, addr)
-			if err != nil {
-				log.Error().Err(err).Str("addr", addr).Msg("[tcp] reconnect failed before TLS 1.2 retry")
-				return nil, err
-			}
-			conf12 := conf.Clone()
-			conf12.MaxVersion = tls.VersionTLS12
-			tlsConn = tls.Client(conn, conf12)
 			if err = tlsConn.Handshake(); err != nil {
-				log.Error().Err(err).Str("addr", addr).Msg("[tcp] TLS 1.2 handshake failed")
-				return nil, err
+				// retry with TLS 1.2 for cameras that don't support TLS 1.3
+				log.Warn().Err(err).Str("addr", addr).Msg("[tcp] TLS 1.3 handshake failed, retrying with TLS 1.2")
+				_ = tlsConn.Close()
+				conn, err = dial(ctx, network, addr)
+				if err != nil {
+					log.Error().Err(err).Str("addr", addr).Msg("[tcp] reconnect failed before TLS 1.2 retry")
+					return nil, err
+				}
+				conf12 := conf.Clone()
+				conf12.MaxVersion = tls.VersionTLS12
+				tlsConn = tls.Client(conn, conf12)
+				if err = tlsConn.Handshake(); err != nil {
+					log.Error().Err(err).Str("addr", addr).Msg("[tcp] TLS 1.2 handshake failed")
+					return nil, err
+				}
+				log.Info().Str("addr", addr).Msg("[tcp] TLS 1.2 handshake succeeded")
+			} else {
+				log.Trace().Str("addr", addr).Msg("[tcp] TLS handshake succeeded")
 			}
-			log.Info().Str("addr", addr).Msg("[tcp] TLS 1.2 handshake succeeded")
-		}
 
 			if pconn, ok := ctx.Value(connKey).(*net.Conn); ok {
 				*pconn = tlsConn
@@ -98,14 +111,19 @@ func Do(req *http.Request) (*http.Response, error) {
 
 	res, err := client.Do(req)
 	if err != nil {
+		log.Error().Err(err).Str("url", req.URL.String()).Msg("[tcp] http request failed")
 		return nil, err
 	}
 
+	log.Debug().Int("status", res.StatusCode).Str("url", req.URL.String()).Msg("[tcp] http response")
+
 	if res.StatusCode == http.StatusUnauthorized && user != nil {
+		log.Debug().Str("url", req.URL.String()).Msg("[tcp] 401 unauthorized, attempting Digest auth")
 		Close(res)
 
 		auth := res.Header.Get("WWW-Authenticate")
 		if !strings.HasPrefix(auth, "Digest") {
+			log.Error().Str("auth", auth).Msg("[tcp] unsupported auth scheme")
 			return nil, errors.New("unsupported auth: " + auth)
 		}
 
@@ -124,12 +142,14 @@ func Do(req *http.Request) (*http.Response, error) {
 
 		switch qop {
 		case "":
+			log.Trace().Str("realm", realm).Msg("[tcp] Digest auth without qop")
 			response := HexMD5(ha1, nonce, ha2)
 			header = fmt.Sprintf(
 				`Digest username="%s", realm="%s", nonce="%s", uri="%s", response="%s"`,
 				username, realm, nonce, uri, response,
 			)
 		case "auth":
+			log.Trace().Str("realm", realm).Msg("[tcp] Digest auth with qop=auth")
 			nc := "00000001"
 			cnonce := core.RandString(32, 64)
 			response := HexMD5(ha1, nonce, nc, cnonce, qop, ha2)
@@ -138,14 +158,18 @@ func Do(req *http.Request) (*http.Response, error) {
 				username, realm, nonce, uri, qop, nc, cnonce, response,
 			)
 		default:
+			log.Error().Str("qop", qop).Msg("[tcp] unsupported Digest qop")
 			return nil, errors.New("unsupported qop: " + auth)
 		}
 
 		req.Header.Set("Authorization", header)
 
 		if res, err = client.Do(req); err != nil {
+			log.Error().Err(err).Str("url", req.URL.String()).Msg("[tcp] http request with Digest auth failed")
 			return nil, err
 		}
+
+		log.Debug().Int("status", res.StatusCode).Str("url", req.URL.String()).Msg("[tcp] http response after Digest auth")
 	}
 
 	return res, nil
